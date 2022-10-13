f11f11515a27c4a410faac31890dc8b5dc92f06e
George Sproston <gsproston@scottlogic.com>
2022-10-13 14:45:33 +0100

    Including the iCal4j library

--- src/main/java/com/scottlogic/app/App.java
+++ src/main/java/com/scottlogic/app/App.java
@@ -1,13 +1,20 @@
 package com.scottlogic.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.FileInputStream;
+
+import net.fortuna.ical4j.data.CalendarBuilder;
+import net.fortuna.ical4j.model.Calendar;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        try {
+            FileInputStream fin = new FileInputStream("mycalendar.ics");
+            CalendarBuilder builder = new CalendarBuilder();
+            Calendar calendar = builder.build(fin);
+            System.out.println(calendar.toString());
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
     }
 }

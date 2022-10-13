a0afac203376c0ac94a34e64ef128166dcc2e765
George Sproston <gsproston@scottlogic.com>
2022-10-13 14:53:43 +0100

    Parsing remote stream to calendars

--- src/main/java/com/scottlogic/app/App.java
+++ src/main/java/com/scottlogic/app/App.java
@@ -1,17 +1,23 @@
 package com.scottlogic.app;
 
-import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.URL;
 
 import net.fortuna.ical4j.data.CalendarBuilder;
 import net.fortuna.ical4j.model.Calendar;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // replace this string with the URL to the iCalendar stream
+        final String STREAM_URL = "<repalce_me>";
+
         try {
-            FileInputStream fin = new FileInputStream("mycalendar.ics");
+            // get the input stream
+            InputStream in = new URL(STREAM_URL).openStream();
+            // parse stream to calendar
             CalendarBuilder builder = new CalendarBuilder();
-            Calendar calendar = builder.build(fin);
+            Calendar calendar = builder.build(in);
+            // print out the calendar
             System.out.println(calendar.toString());
         } catch (Exception e) {
             System.err.println("Error: " + e.getMessage());
